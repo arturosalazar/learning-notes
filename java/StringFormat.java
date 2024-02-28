b9f831cd9021635ef335e85dd771cd8ae63f979a
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-27 19:27:00 -0600

    Add StringFormat.java to demonstrate how to use String.format() with different values

---
+++ java/StringFormat.java
@@ -0,0 +1,7 @@
+// Demonstrates the use of String.format() for formatting strings in Java.
+// This example shows how to format numerical values into a more readable string format,
+// especially useful for displaying user-friendly messages or logging.
+
+public class StringFormat {
+
+}
