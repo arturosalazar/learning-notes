ba53eadb9a7074f28a48839a29b4fae90c451ecd
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-27 21:15:58 -0600

    Add main function, demonstrating class vs local scope

--- java/VariableScope.java
+++ java/VariableScope.java
@@ -1,4 +1,13 @@
 // Demonstrates variable scope in Java, highlighting the accessibility of variables based on where they are defined.
 public class VariableScope {
-    
+    // Static variable with class scope, accessible from anywhere within the class.
+    static String outer = "Outer: Global Scope";
+
+    public static void main(String[] args) {
+        System.out.println("Inside main: can access the static variable: " + outer);
+
+        // Local variable to main method.
+        String inMain = "inMain: Local to main";
+        System.out.println("Inside main: can access 'inMain': " + inMain);
+    }
 }
