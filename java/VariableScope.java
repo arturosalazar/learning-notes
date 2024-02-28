8cfdb8586aa86a32f792aef101e64390a83c1907
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-27 21:16:59 -0600

    Add method1 to demonstrate local variables being inaccessible inside other methods

--- java/VariableScope.java
+++ java/VariableScope.java
@@ -9,5 +9,17 @@ public class VariableScope {
         // Local variable to main method.
         String inMain = "inMain: Local to main";
         System.out.println("Inside main: can access 'inMain': " + inMain);
+
+        // Calling other methods within the class.
+        method1();
+    }
+
+    public static void method1() {
+        // Accessible due to class scope.
+        System.out.println("Inside method1: can access the static variable: " + outer);
+
+        // inMain is not accessible here; it's local to main.
+        String inMethod1 = "inMethod1: Local to method1";
+        System.out.println("Inside method1: can access 'inMethod1': " + inMethod1);
     }
 }
