127de2584c4a4fcf2aa3734dc6b4a7176d29ace0
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-04-18 23:38:20 -0500

    Update comments to describe program and standard for loop

--- java/stringmanipulation/IterateOverStrings.java
+++ java/stringmanipulation/IterateOverStrings.java
@@ -1,10 +1,17 @@
 package stringmanipulation;
-// Demonstrates how to iterate over a String in Java 
+// Demonstrates a variety of ways to iterate over a String in Java
+
+// Simply printing the character will be used to demo processing the character, but any other way to process can be used
+// When applicable, reasons behind using one way over another way to iterate will be explained
 
 public class IterateOverStrings {
+
     public static void main(String[] args) {
-        // Loop over string with a for loop
         String example = "This is some example text";
+
+        // Using a standard 'for' loop with charAt():
+        // Pro: Straightforward, allows you to keep track of index
+        // Con: Lots of boilerplate code to provide
         for (int i = 0; i < example.length(); i++) {
             System.out.println(example.charAt(i));
         }
