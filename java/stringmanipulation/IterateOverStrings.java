65bb4eea531a86926f32079f1938ed7b7cad01c9
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-04-18 23:39:22 -0500

    add while loop example

--- java/stringmanipulation/IterateOverStrings.java
+++ java/stringmanipulation/IterateOverStrings.java
@@ -15,5 +15,17 @@ public class IterateOverStrings {
         for (int i = 0; i < example.length(); i++) {
             System.out.println(example.charAt(i));
         }
+        System.out.println();
+
+        // Using a standard 'while' loop with charAt():
+        // Pro: Straightforward, allows you to keep track of index
+        // Con: Lots of boilerplate code to provide, need to create index variable
+        // and remember to keep iterating index
+        int i = 0;
+        while (i < example.length()) {
+            System.out.print(example.charAt(i));
+            i++;
+        }
+        System.out.println();
     }
 }
